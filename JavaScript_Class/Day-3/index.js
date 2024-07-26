0c38aff0cad35c65f803d094577de1e126eb3b85
anandgithub011 <anandany830@gmail.com>
2024-07-26 08:10:29 +0530

    Output Methods

---
+++ JavaScript_Class/Day-3/index.js
@@ -0,0 +1,107 @@
+console.log("Hi JavaScript");
+
+
+// Output Method:--------------------//
+
+// 1. console.log();      //console is a object and log() is a method and also called output method, which is used for testing purpose.
+
+// console.log("Anand");
+
+
+
+
+// 2. document.write();    //document is a object and write() is a method and it is used for display the content in the browser.
+
+// document.write("Hi I am a Document.");
+// document.write("<h1>Hi I am a Document.</h1>");
+// document.write("Hi I am a Document.");
+// document.write("<br>");
+// document.write("Hi I am a Document.");
+// document.write("<br>");
+// document.write("Hi I am a Document.");
+
+
+// 3. confirm();  // the confirm() methods displays a dialog box like a pop-up and it comes with a message with two buttons, 1st one is ok button and 2nd one is cancel button.
+
+// let task = confirm("Are you above 18 years or Not!!!");
+// console.log(task);
+
+// if(task) {
+//     console.log("Hi Welcome to the Website..");
+// }else{
+//     console.log("Hey Idiot You can't Access.!!!!");
+// }
+
+
+// 4. alert();  // The alert() method in javaScript is used to display a virtual alert box. It is mostly used to give a warning message to the users.
+
+// let task = confirm("Are you above 18 years or Not!!!");
+// console.log(task);
+
+// if(task) {
+//     alert("Hi Welcome to the Website..");
+// }else{
+//     alert("Hey Idiot You can't Access.!!!!");
+// }
+
+
+// 5. prompt();   //the prompt() method is used to collect the data from users (dynamic data).
+
+// let name1 = prompt("Hi Sir, Please Enter Your Name");
+
+// console.log(name1);
+// var a;
+// console.log(a)
+
+// console.log(2-"1");
+// let a = ["hi","hbs","djhj"];
+// a[2] = "sssss";
+// console.log(a);
+
+// let a = 23;
+// a = 244;
+// console.log(a);
+// console.info(a);
+
+// pre increment-----------------
+// let a = 7;
+// ++a;
+// console.log(a);
+
+// console.log(typeof (true+false));  // number
+// console.log(typeof(true+"ih")); //string
+
+// console.log(typeof (NaN+4)); //number
+
+// console.log(3 - - "2"); //5
+// console.log(typeof (3 - - "2")); //number
+
+// console.log(3 * - "3"); //-9
+// console.log(typeof (3 * - "3")); //number
+
+// console.log(typeof ("" == true)); // boolean
+
+// console.log(typeof Boolean); // function
+// console.log("ghj");
+
+// var b = 5;
+
+// var b = 6;
+
+// console.log(b);
+
+// let b = "Go";
+
+// var b = 567;
+
+// console.log(b);
+
+// let break = 10; // uncaught syntax error
+
+
+let x = 10;
+let y = 100;
+// ++x; 
+console.log(++x); //x++;
+
+
