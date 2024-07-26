1e8b5f662be4f6c92510d28c3fc70080fe783706
anandgithub011 <anandany830@gmail.com>
2024-07-26 08:09:50 +0530

    Assignment Operators

---
+++ JavaScript_Class/Day-6/index.js
@@ -0,0 +1,65 @@
+console.log("Hi JavaScript");
+
+// let x = 10;
+// console.log(x);
+
+// let y;
+// y = x;
+// console.log(y);
+
+
+// ==========ASSIGNMENT OPERATOR========================//
+
+
+// ======= (+=) operator  =========== //
+
+// let x = 100;
+
+// // x += 10;
+// x = x+10;
+// console.log(x);
+
+
+// ===========  (-=) operator  ==========//
+// let x = 100;
+
+// // x -= 50;
+// x = x-50;
+// console.log(x);
+
+
+// ========  (*=) operator  ===============
+
+// let x = 100;
+// // x *= 5;
+
+// x = x * 5;
+// console.log(x);
+
+
+// ============  (/=) operator ====================
+
+// let x = 100;
+// // x /= 10;
+
+// x = x / 10;
+// console.log(x);
+
+// =========  (%=) operator ===================//
+
+// let x = 100;
+
+// // x %= 9;
+
+// x = x % 9;
+// console.log(x);
+
+// =========  (**=) operator ===================//
+
+let x = 10;
+let y = 2;
+x **= y; 
+
+// x = x ** y; //  10^2 = 100;
+
+console.log(x);
