82f9e32213739a506eee07086ba823225e7d28d3
anandgithub011 <anandany830@gmail.com>
2024-07-28 16:00:28 +0530

    JavaScipt Comparison Operatoe

---
+++ JavaScript_Class/Day-7/index.js
@@ -0,0 +1,140 @@
+console.log("Hi JavaScript");
+
+// JavaSCript Comparison Operators --------------------//
+// 1. (==)equal to ---------------------//
+// 2. (===)equal value and equal type --------------------------//
+// 3. (!=) not equal -------------------------------//
+// 4. (!==) not equal value or not equal type-----------------------//
+// 5. (>) greater than--------------------------//
+// 6. (<) less than-----------------------------//
+// 7. (>=) greater than or equal to------------------------//
+// 8. (<=) less than or equal to----------------------//
+// 9. Ternary operators -------------------------------//
+
+
+
+
+// 1. (==)equal to ---------------------//
+
+// let x = 100;
+// let y = 100;
+// console.log(x == y); //true
+
+// let x = 100;
+// let y = 10; 
+// console.log(x == y);  //false
+
+
+
+// 2. (===)equal value and equal type --------------------------//
+
+// let x = 100;
+// let y = 100;
+// console.log(x === y); // true
+
+
+// let x = 100;
+// let y = "100";
+// console.log(x === y);  //false
+
+// let x = "100";
+// let y = "100";
+// console.log(x === y);  //true
+
+// let x = "200";
+// let y = "100";
+// console.log(x === y);  //false
+
+
+
+// 3. (!=) not equal -------------------------------//
+
+// let x = 100;
+// let y = 100;
+// console.log(x != y); //false
+
+// let x = 100;
+// let y = "100";
+// console.log(x != y); //false
+
+// let x = 100;
+// let y = 200;
+// console.log(x != y); //true
+
+
+// 4. (!==) not equal value or not equal type-----------------------//
+
+// let x = 100;
+// let y = "100";
+// console.log(x !== y); //true
+
+// let x = 100;
+// let y = 100;
+// console.log(x !== y); //false
+
+
+
+// 5. (>) greater than--------------------------//
+
+// let x = 100;
+// let y = 200;
+// console.log(x > y); //false
+
+// let x = 200;
+// let y = 100;
+// console.log(x > y); //true
+
+
+// 6. (<) less than-----------------------------//
+
+// let x = 100;
+// let y = 200;
+// console.log(x < y); //true
+
+// let x = 200;
+// let y = 100;
+// console.log(x < y); //false
+
+
+// 7. (>=) greater than or equal to------------------------//
+
+// let x = 100;
+// let y = 200;
+// console.log(x >= y); //false
+
+// let x = 100;
+// let y = 100;
+// console.log(x >= y); //true
+
+// let x = 200;
+// let y = 100;
+// console.log(x >= y); //true
+
+
+
+// 8. (<=) less than or equal to----------------------//
+
+// let x = 100;
+// let y = 200;
+// console.log(x <= y); //true
+
+// let x = 100;
+// let y = 100;
+// console.log(x <= y); //true
+
+// let x = 200;
+// let y = 100;
+// console.log(x <= y); //false
+
+
+// 9. Ternary operators -------------------------------//
+
+// let age = 20;
+
+// if(age >= 20){
+//     console.log("You can Access...");
+// }else{
+//     console.log("You Can't Access!!!!!");
+// }
+
+// (age >= 20) ? console.log("You can Access..."): console.log("You Can't Access!!!!!");n ni
