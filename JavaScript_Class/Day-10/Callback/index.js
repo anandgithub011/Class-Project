138aa57bb00a6c37e1bc798c90ba991a5388ae2c
anandgithub011 <anandany830@gmail.com>
2024-08-02 08:37:45 +0530

    Example of Callback function

---
+++ JavaScript_Class/Day-10/Callback/index.js
@@ -0,0 +1,19 @@
+console.log("Hi JavaScript");
+
+//============== Examples of Higher order function and callback function.=================================//
+
+// function demo(a,b,callback){
+
+//     console.log(a);
+//     console.log(b);
+//     callback(100,200);
+
+// }
+
+// demo(10,20, function(x,y){
+
+//     console.log("Hiiiiiiiii");
+//     console.log(x);
+//     console.log(y);
+
+// })
