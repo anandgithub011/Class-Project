6c6ddc71d7f77dc62e971f497b7cd47de6228e6c
anandgithub011 <anandany830@gmail.com>
2024-08-09 23:52:41 +0530

    arr.includes

---
+++ JavaScript-2.0/Array-Day-1/inbuilt-function'/arr.includes/index.js
@@ -0,0 +1,40 @@
+
+
+
+
+// includes(arg1,arg2) Method:
+// It is used to check whether the given element is present or not inside the Array.
+// It accepts two arguments where the 1st-argument is element to searching purpose, 2nd-argument is index-position from where to start the search of element.
+
+// If we dont pass 2nd-argumrnt then its start searching the element from 0-index.
+// It returns a boolean value true, if the element is present else, it returns false.
+// It doesn't affect the original Array
+
+
+
+
+
+
+
+// includes(arg1,arg2) Method:Determines whether an array includes a certain element, returning true or false as appropriate.
+
+// @param searchElement — The element to search for.
+
+// @param fromIndex — The position in this array at which to begin searching for searchElement.
+
+
+
+
+
+
+let colors = ['red','green','blue','pink','yellow','orange'];
+
+
+let slicedElement1 = colors.includes('blue',3);  
+let slicedElement2 = colors.includes('blue');  
+
+
+// console.log(colors);  //  (6)['red','green','blue','pink','yellow','orange'];
+
+console.log(slicedElement1);  // false
+console.log(slicedElement2);  // true
