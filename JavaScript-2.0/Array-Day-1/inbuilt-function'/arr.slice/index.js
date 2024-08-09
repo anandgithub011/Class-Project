7f6e6d740cdfa1992c9d884f0af9ea76d03e3de7
anandgithub011 <anandany830@gmail.com>
2024-08-09 23:17:04 +0530

    arr.slice

---
+++ JavaScript-2.0/Array-Day-1/inbuilt-function'/arr.slice/index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+let colors = ['red','green','blue','pink','yellow','orange'];
+
+
+let slicedElement = colors.slice(2,5);  
+
+
+console.log(colors);  //  (6)['red','green','blue','pink','yellow','orange'];
+
+console.log(slicedElement);  // (3) ['blue','pink','yellow']
