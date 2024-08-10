6eeec3cc8f51ca8b2315964f38c97284fddb2d15
anandgithub011 <anandany830@gmail.com>
2024-08-10 06:38:00 +0530

    arr.reverse

---
+++ JavaScript-2.0/Array-Day-1/inbuilt-function'/arr.reverse/index.js
@@ -0,0 +1,41 @@
+
+
+
+// reverse() Method :
+// It is used to reverse the Array element.
+// It does not have any argument.
+// It returns the Original array with reverse elements.
+// It affects the original Array.
+
+
+
+
+// ==================================================================================================================
+
+
+// reverse() Method : Reverses the elements in an array in place. This method mutates the array and returns a reference to the same array.
+
+
+// ==================================================================================================================
+
+
+
+
+
+
+
+let colors = ['red','green','blue','pink','yellow','orange'];
+
+console.log(colors); //(6) ['red','green','blue','pink','yellow','orange']
+
+
+let reverseArr = colors.reverse();
+
+
+
+
+console.log(reverseArr);   //['orange'','yellow','pink','blue','green''red']
+console.log(colors);    //['orange'','yellow','pink','blue','green''red']
+
+
+
