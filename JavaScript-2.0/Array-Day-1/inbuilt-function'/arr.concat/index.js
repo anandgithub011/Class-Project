664f3cc35f5a02838f643e46d86d3765d16b3d57
anandgithub011 <anandany830@gmail.com>
2024-08-11 12:23:00 +0530

    arr.concat()

--- JavaScript-2.0/Array-Day-1/inbuilt-function'/arr.concat/index.js
+++ JavaScript-2.0/Array-Day-1/inbuilt-function'/arr.concat/index.js
@@ -1,5 +1,8 @@
 
-// concat(arg1,....) Method:
+//! concat(arg1,....) Method:
+
+
+//* concat(arg1,....) Method:
 // It is used to join the new elements to the existing array, after the last index of an Array.
 // It accepts n-numbers of arguments, where the arguments are new elements to be joined.
 // It returns an array of new joined element.
@@ -8,12 +11,11 @@
 
 
 // ======================================================================================================================
-
-// concat(arg1,....) Method:
+//* concat(arg1,....) Method:
+// (method) Array<string>.concat(...items: (string | ConcatArray<string>)[]): string[] (+1 overload)
 // Combines two or more arrays. This method returns a new array without modifying any existing arrays.
 
 // @param items — Additional arrays and/or items to add to the end of the array.
-
 
 // ======================================================================================================================
 
@@ -21,16 +23,16 @@
 
 let colors = ['red','green','blue','pink','yellow','orange'];
 
-console.log(colors); //(6) ['red','green','blue','pink','yellow','orange']
+console.log(colors); //*(6) ['red','green','blue','pink','yellow','orange']
 
 
 let joinedElement1 = colors.concat('xyz','mno');
 let joinedElement2 = colors.concat(['jkl','abc','pqr']);
 
 
-console.log(joinedElement1); // (8) ['red','green','blue','pink','yellow','orange','xyz','mno']
+console.log(joinedElement1); //* (8) ['red','green','blue','pink','yellow','orange','xyz','mno']
 
-console.log(joinedElement2); //(9) ['red','green','blue','pink','yellow','orange','jkl','abc','pqr']
+console.log(joinedElement2); //*(9) ['red','green','blue','pink','yellow','orange','jkl','abc','pqr']
 
 
-console.log(colors); ////(6) ['red','green','blue','pink','yellow','orange']
+console.log(colors); //*(6) ['red','green','blue','pink','yellow','orange']
