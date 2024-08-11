d31350c53ce26f94d4b596ca77fdbe51e6ee677b
anandgithub011 <anandany830@gmail.com>
2024-08-11 12:22:36 +0530

    arr.at()

--- JavaScript-2.0/Array-Day-1/inbuilt-function'/arr.at/index.js
+++ JavaScript-2.0/Array-Day-1/inbuilt-function'/arr.at/index.js
@@ -1,6 +1,7 @@
 
+//! at(arg1) Method:
 
-// at(arg1) Method:
+//* at(arg1) Method:
 // It is used to get the element of array, based on their index-position
 // 
 // It accepts only one argument, where the argument is index-position.
@@ -14,18 +15,17 @@
 
 let colors = ['red','green','blue','pink','yellow','orange'];
 
-console.log(colors); //(6) ['red','green','blue','pink','yellow','orange'];
+console.log(colors); //*(6) ['red','green','blue','pink','yellow','orange'];
 
 
 let element1 = colors.at(3);  
+console.log(element1);  //*pink
+
 let element2 = colors.at(8);  
-// let element3 = colors.at(-2);  
+console.log(element2);  //* undefined  (by default)
+
+let element3 = colors.at(-2);    //*(taking last index as a -1 and goes backward direction. so, -2 is yellow)
+console.log(element3);  //* yellow 
 
 
-
-console.log(element1);  //pink
-console.log(element2);  // undefined  (by default)
-// console.log(element3);  // orange 
-
-
-console.log(colors); // //(6) ['red','green','blue','pink','yellow','orange'];
+console.log(colors); //* (6) ['red','green','blue','pink','yellow','orange'];
