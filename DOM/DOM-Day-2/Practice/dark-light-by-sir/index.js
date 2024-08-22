261f014967e94e0ff458acc46b3de49efe5ea0e7
anandgithub011 <anandany830@gmail.com>
2024-08-22 23:24:10 +0530

    dark-light-by-sir

---
+++ DOM/DOM-Day-2/Practice/dark-light-by-sir/index.js
@@ -0,0 +1,14 @@
+
+
+function lightDark() {
+  let btn = document.getElementById("btn");
+  let toggle = document.getElementById("toggle");
+
+  toggle.classList.toggle("dark");
+  document.body.classList.toggle("bg-dark");
+  btn.classList.toggle("btn-dark");
+}
+
+
+
+
