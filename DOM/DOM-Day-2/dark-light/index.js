73f11fecd1a2df969c8ae0b5c7bc3b47942dcadf
anandgithub011 <anandany830@gmail.com>
2024-08-22 19:04:33 +0530

    toggle-Dark-light

---
+++ DOM/DOM-Day-2/dark-light/index.js
@@ -0,0 +1,23 @@
+let btn = document.getElementById("btn");
+let btnContainer = document.getElementById("btnContainer");
+document.body.style.backgroundColor = "white";
+document.body.style.color = "black";
+btn.innerText = "Dark";
+btn.className = "btn2";
+btnContainer.className = "btn-container2";
+
+function darkLight() {
+  if (btn.innerText === "Dark") {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    btn.innerText = "Light";
+    btn.className = "btn1";
+    btnContainer.className = "btn-container1";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    btn.innerText = "Dark";
+    btn.className = "btn2";
+    btnContainer.className = "btn-container2";
+  }
+}
