0a74c1046b800dc63ac51a97f5886de297c320a5
anandgithub011 <anandany830@gmail.com>
2024-08-25 06:17:03 +0530

    Pic-Assignment

---
+++ DOM/DOM-Day-4/Class-work/Assign -1/index.js
@@ -0,0 +1,52 @@
+
+let rightCard = document.getElementById("rightCard");
+
+function Card(e){
+    rightCard.src = e.target.src;       
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Card1(e){
+//     rightCard.src = e.target.src
+// }
+// function Card2(e){
+//     rightCard.src = e.target.src
+// }
+// function Card3(e){
+//     rightCard.src = e.target.src
+// }
+// function Card4(e){
+//     rightCard.src = e.target.src
+// }
